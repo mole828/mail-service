15b229acbc9a188dc078a633da09dcc104720cae
mole828 <mrx800@163.com>
2024-04-01 22:07:21 +0800

    feat: send email

--- main.go
+++ main.go
@@ -3,28 +3,72 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"log"
+	"net/smtp"
 	"os"
 
 	"github.com/google/uuid"
 )
 
-type MailServiceConfig struct {
-	Token string
+type Mailer struct {
+	SmtpServer string
+	Port       string
+	Sender     string
+	Password   string
 }
 
-func ReadConfig(path string) *MailServiceConfig {
+func (m *Mailer) SendEmail(to string, message string) {
+	// 创建邮件
+	msg := []byte("To: " + to + "\r\n" +
+		"Subject: Golang Mail Service \r\n" +
+		"\r\n" +
+		message + "\r\n")
+
+	// 发送邮件
+	auth := smtp.PlainAuth("", m.Sender, m.Password, m.SmtpServer)
+	err := smtp.SendMail(m.SmtpServer+":"+m.Port, auth, m.Sender, []string{to}, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type MailServiceConfig struct {
+	Token  string
+	Mailer Mailer
+}
+
+func ReadConfig(path string) (*MailServiceConfig, error) {
 	var mail_service_config = new(MailServiceConfig)
-	file, open_err := os.Open("/etc/mail/config.json")
+	file, open_err := os.Open(path)
 	if os.IsNotExist(open_err) {
 		mail_service_config.Token = uuid.New().String()
-		file, _ := os.Create(path)
-		var bytes, _ = json.Marshal(mail_service_config)
-		file.Write(bytes)
+		file, create_err := os.Create(path)
+		if create_err != nil {
+			return nil, create_err
+		}
+		defer file.Close()
+		bytes, marshal_err := json.Marshal(mail_service_config)
+		if marshal_err != nil {
+			return nil, marshal_err
+		}
+		_, write_err := file.Write(bytes)
+		if write_err != nil {
+			return nil, write_err
+		}
+	} else if open_err != nil {
+		return nil, open_err
+	} else {
+		defer file.Close()
+		bytes, read_err := io.ReadAll(file)
+		if read_err != nil {
+			return nil, read_err
+		}
+		unmarshal_err := json.Unmarshal(bytes, mail_service_config)
+		if unmarshal_err != nil {
+			return nil, unmarshal_err
+		}
 	}
-	defer file.Close()
-	bytes, _ := io.ReadAll(file)
-	json.Unmarshal(bytes, mail_service_config)
-	return mail_service_config
+	return mail_service_config, nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -3,5 +3,17 @@ package main
 import "testing"
 
 func TestReadConfig(t *testing.T) {
-	t.Log(ReadConfig("./config.json"))
+	config, err := ReadConfig("./test/config.json")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("%+v", config)
+}
+
+func TestSend(t *testing.T) {
+	config, err := ReadConfig("./test/config.json")
+	if err != nil {
+		t.Error(err)
+	}
+	config.Mailer.SendEmail("mrx800@163.com", "smtp test")
 }
